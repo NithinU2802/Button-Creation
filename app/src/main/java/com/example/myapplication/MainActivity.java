02d5e78d4d8ac7ba97ae5da378507c19d06ce964
Nithin <nithinninja2801@gmail.com>
2023-01-22 01:24:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b=findViewById(R.id.button);
+
+    }
+    public void text(View view){
+        Intent i=new Intent(this,MyOtherActivity.class);
+        startActivity(i);
+    }
+
+}
